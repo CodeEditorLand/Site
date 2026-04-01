991aa4ba16c240cfd3597e9b3e6394bd05a73b32
Nikola Hristov <Nikola@PlayForm.Cloud>
2026-04-01 10:34:05 +0300

    save
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Source/Component/ErrorBoundary.tsx
+++ Source/Component/ErrorBoundary.tsx
@@ -27,7 +27,7 @@ export class ErrorBoundary extends Component<
 		return { hasError: true, error };
 	}
 
-	componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+	override componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
 		console.error("ErrorBoundary caught an error:", error, errorInfo);
 	}
 
