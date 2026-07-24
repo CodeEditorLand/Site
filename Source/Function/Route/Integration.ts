0f6c989f04f594d8778105c51401ed7b960d5984
Nikola Hristov <Nikola@PlayForm.Cloud>
2026-07-24 17:11:45 +0300

    chore: remove /jelly.js from redirects (now in _astro/ via Vite)

--- Source/Function/Route/Integration.ts
+++ Source/Function/Route/Integration.ts
@@ -306,7 +306,6 @@ const RouteRedirectIntegration = (): AstroIntegration => ({
 				"/sitemap-index.xml",
 				"/sitemap-0.xml",
 				"/robots.txt",
-				"/jelly.js",
 			];
 
 			const Pad = (Value: string, Width: number): string =>
